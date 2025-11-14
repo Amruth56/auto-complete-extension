a666a905442f07ec32369585db2b0d7b391228b7
Amruth <ammumandappa@gmail.com>
2025-11-14 22:55:15 +0530

    cerated files and wrote the env file

---
+++ src/env.ts
@@ -0,0 +1,7 @@
+
+
+export const DEFAULT_API_KEY = "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";
+
+export const OPENROUTER_URL = "https://openrouter.ai/api/v1/chat/completions";
+
+export const MODEL_NAME = "google/gemini-2.0-flash-exp:free";
